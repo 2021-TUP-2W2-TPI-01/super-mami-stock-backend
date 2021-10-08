eaccaa4231a23306e8b784d819867d75e0028235
Emiliano <112063@tecnicatura.frc.utn.edu.ar>
2021-10-07 23:21:39 -0300

    Alta Usuario from PullRequest

--- control_stock_app/controllers/usuario_controller.py
+++ control_stock_app/controllers/usuario_controller.py
@@ -41,29 +41,17 @@ def obtener_usuarios():
     return lstUsuarios
 
 
-def alta_usuario(request):
+def alta_usuario(**args):
+    _username = args.get('username')
+    _password = args.get('password')
+    _email = args.get('email')
+    _last_name = args.get('last_name')
+    _first_name = args.get('first_name')
     try:
-        _username = request.POST['username']
-        _password = request.POST['password']
-        _email = ""
-        _last_name = ""
-        _first_name = ""
-        try:
-            _email = request.POST['email']
-            _last_name = request.POST['last_name']
-            _first_name = request.POST['_first_name']
-        except:
-            pass
-        try:
-            if _username != None and _password != None:
-                User.objects.create(username = _username, password = _password,email= _email,
-                last_name = _last_name, first_name = _first_name)
-                return Response('Usuarios creados correctamente', status=status.HTTP_201_CREATED)
-            else:
-                return Response('Debe Ingresar los campos obligatorios', status=status.HTTP_400_BAD_REQUEST)
-        except Exception as ex:
-                print(ex)
-                return Response('Existe un usuario con ese username, reintente', status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e:
-            return Response('Datos Insuficientes', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        User.objects.create(username = _username, password = _password,email= _email,
+        last_name = _last_name, first_name = _first_name)
+        return True
+    except:
+        return False
+                
     
--- control_stock_app/views.py
+++ control_stock_app/views.py
@@ -52,9 +52,37 @@ class Usuario(APIView):
         pass
 
     def post(self, request):
-        return alta_usuario(request)
+        try:
+            _username = request.POST['username']
+            _password = request.POST['password']
+            _email = ""
+            _last_name = ""
+            _first_name = ""
+            try:
+                _email = request.POST['email']
+            except:
+                pass
+            try:
+                _last_name = request.POST['last_name']
+            except:
+                pass
+            try: 
+                _first_name = request.POST['_first_name']
+            except:
+                pass
+            try:
+                if _username != None and _password != None:
+                    if alta_usuario(username = _username, password = _password,email= _email,
+                    last_name = _last_name, first_name = _first_name):
+                        return Response('Usuarios creados correctamente', status=status.HTTP_201_CREATED)
+                else:
+                    return Response('Debe Ingresar los campos obligatorios', status=status.HTTP_400_BAD_REQUEST)
+            except Exception as ex:
+                    print(ex)
+                    return Response('Existe un usuario con ese username, reintente', status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+                return Response('Datos Insuficientes', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def delete(self, request, pk):
-
         pass
 
 @api_view(['GET'])
