9e32a31693092def816f1673fb01243195b2197a
Benjamin Conti <contibenjamin@gmail.com>
2021-10-08 16:02:01 -0300

    Método get_localidades

---
+++ control_stock_app/controllers/deposito_controller.py
@@ -0,0 +1,10 @@
+# Aca van los metodos referidos a depósitos (obtener un depósito, obtener todos los depósitos, alta despósito, baja despósito, modificacion despósito)
+from django.http import response
+from ..models import *
+
+
+def obtener_localidades():
+
+    localidades = Localidades.objects.all()
+
+    return localidades
