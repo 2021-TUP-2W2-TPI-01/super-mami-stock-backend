d4174a7b9274df44509c60ffe07240e1d3896cd3
Akio Tamashiro <angelakio.tamashiro@gmail.com>
2021-10-16 17:34:34 -0300

    Fix traspasos_controller (se deja todo en un solo  controller)

--- control_stock_app/controllers/traspasos_controller.py
+++ control_stock_app/controllers/traspasos_controller.py
@@ -4,6 +4,40 @@ from ..models import *
 from django.db import connection
 from ..data_access import db_helper as _db
 
+
+def alta_traspaso(traspaso, detalle_traspaso):
+    try:
+
+        deposito_origen = Depositos.objects.get(id = traspaso.id_deposito_origen)
+
+        deposito_destino = Depositos.objects.get(id = traspaso.id_deposito_destino)
+
+        ultimo_traspaso = Traspasos.objects.create(fh_generacion = datetime.now(), id_tipo_estado_id = 1, id_deposito_origen = deposito_origen, id_deposito_destino = deposito_destino, id_usuario_genero_id = traspaso.id_usuario_genero)
+
+        lstDetalles = []
+
+        for detalle in detalle_traspaso:
+            id_articulo = detalle['id_articulo']
+            cantidad_articulo = detalle['cantidad']
+
+            det_traspaso = DetallesTraspaso()
+
+            det_traspaso.id_articulo_id = id_articulo
+            det_traspaso.cantidad = cantidad_articulo
+
+            det_traspaso.id_traspaso = ultimo_traspaso
+
+            lstDetalles.append(det_traspaso)
+        
+        DetallesTraspaso.objects.bulk_create(lstDetalles)      
+    
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+
 def obtener_traspasos_al_deposito(usuario):
 
     lstTraspasos = []
