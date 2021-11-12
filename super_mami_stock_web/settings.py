d04baf23c7a2c5e83a113cfca8208243f2f55ec4
Akio Tamashiro <angelakio.tamashiro@gmail.com>
2021-11-12 12:42:02 -0300

    secret key

--- super_mami_stock_web/settings.py
+++ super_mami_stock_web/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&y05%gvqzhs&-(@7lchfltqmy5qgg2v#q%a&6!a4zn_bmoyog%'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
